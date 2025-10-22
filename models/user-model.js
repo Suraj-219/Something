771e8c40ce619245b4a011c69c6162cb1dc23088
Suraj Pal <surajpal7442@gmail.com>
2025-10-22 17:05:08 +0530

    update js

--- models/user-model.js
+++ models/user-model.js
@@ -3,11 +3,26 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
     fullname: {
         type: String,
-        miniLength: 3,
+        required: true,
+        lowercase: true,
+        unique: true,
+        miniLength: [3, 'Fullname must be at least 3 characters long'],
         trim: true,
     },
-    email: String,
-    password: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        miniLength: [13, 'Email must be at least 13 characters long'],
+    },
+    password: {
+        type: String,
+        required: true,
+        trim : true,
+        miniLength: [5, 'Password must be at least 5 characters long'],
+    },
     cart: [
         {
         type: mongoose.Schema.Types.ObjectId,
